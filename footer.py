5f38d15ae93d9105c369e888ca8708872634514d
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2010-04-20 17:31:52 +0200

    python: EventType have been renamed to Event

--- footer.py
+++ footer.py
@@ -28,7 +28,7 @@ class EventUnion(ctypes.Union):
 
 class Event(ctypes.Structure):
     _fields_ = [
-        ('type', EventType),
+        ('type', Event),
         ('object', ctypes.c_void_p),
         ('u', EventUnion),
         ]
