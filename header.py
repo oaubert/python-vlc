6192d04dd22c0ef10bee0610ef560f01b8b9de13
Jean Brouwers <MrJean1@EarthLink.net>
2010-12-07 18:18:44 +0100

    python-vlc: update structure mapping
    Signed-off-by: Olivier Aubert <olivier.aubert@liris.cnrs.fr>

--- header.py
+++ header.py
@@ -176,135 +176,127 @@ class ListPOINTER(object):
 # GENERATED_ENUMS go here  # see generate.py
  # End of generated enum types #
 
-# From libvlc_structures.h
+ # From libvlc_structures.h
 
-class MediaStats(ctypes.Structure):
-    _fields_= [
-                ('read_bytes',          ctypes.c_int  ),
-                ('input_bitrate',       ctypes.c_float),
-                ('demux_read_bytes',    ctypes.c_int  ),
-                ('demux_bitrate',       ctypes.c_float),
-                ('demux_corrupted',     ctypes.c_int  ),
-                ('demux_discontinuity', ctypes.c_int  ),
-                ('decoded_video',       ctypes.c_int  ),
-                ('decoded_audio',       ctypes.c_int  ),
-                ('displayed_pictures',  ctypes.c_int  ),
-                ('lost_pictures',       ctypes.c_int  ),
-                ('played_abuffers',     ctypes.c_int  ),
-                ('lost_abuffers',       ctypes.c_int  ),
-                ('sent_packets',        ctypes.c_int  ),
-                ('sent_bytes',          ctypes.c_int  ),
-                ('send_bitrate',        ctypes.c_float),
-                ]
+class AudioOutput(ctypes.Structure):
 
     def __str__(self):
-        return "\n".join( [ self.__class__.__name__ ]
-                          + [" %s:\t%s" % (n, getattr(self, n)) for n in self._fields_] )
+        return '%s(%s:%s)' % (self.__class__.__name__, self.name, self.description)
 
-class MediaTrackInfo(ctypes.Structure):
-    _fields_= [
-        ('codec'   , ctypes.c_uint32),
-        ('id'      , ctypes.c_int),
-        ('type'    , TrackType),
-        ('profile' , ctypes.c_int),
-        ('level'   , ctypes.c_int),
-        ('channels_or_height',  ctypes.c_uint),
-        ('rate_or_width'    , ctypes.c_uint),
-        ]
+    def __repr__(self):
+        return '%s.%s' % (self.__class__.__module__, self.__str__())
 
-    def __str__(self):
-        return "\n".join( [self.__class__.__name__]
-                          + [" %s:\t%s" % (n, getattr(self, n)) for n in self._fields_])
-
-class PlaylistItem(ctypes.Structure):
-    _fields_= [
-                ('id', ctypes.c_int),
-                ('uri', ctypes.c_char_p),
-                ('name', ctypes.c_char_p),
-                ]
-
-    def __str__(self):
-        return "%s #%d %s (uri %s)" % (self.__class__.__name__, self.id, self.name, self.uri)
+AudioOutput._fields_ = [  # recursive struct
+        ('name',        ctypes.c_char_p),
+        ('description', ctypes.c_char_p),
+        ('next',        ctypes.POINTER(AudioOutput)),
+    ]
 
 class LogMessage(ctypes.Structure):
-    _fields_= [
-                ('size', ctypes.c_uint),
-                ('severity', ctypes.c_int),
-                ('type', ctypes.c_char_p),
-                ('name', ctypes.c_char_p),
-                ('header', ctypes.c_char_p),
-                ('message', ctypes.c_char_p),
-                ]
+    _fields_ = [
+        ('size',     ctypes.c_uint  ),
+        ('severity', ctypes.c_int   ),
+        ('type',     ctypes.c_char_p),
+        ('name',     ctypes.c_char_p),
+        ('header',   ctypes.c_char_p),
+        ('message',  ctypes.c_char_p),
+    ]
 
     def __init__(self):
         super(LogMessage, self).__init__()
-        self.size=ctypes.sizeof(self)
+        self.size = ctypes.sizeof(self)
 
     def __str__(self):
-        return "%s(%d:%s): %s" % (self.__class__.__name__, self.severity, self.type, self.message)
+        return '%s(%d:%s): %s' % (self.__class__.__name__, self.severity, self.type, self.message)
 
-
-class AudioOutput(ctypes.Structure):
-    def __str__(self):
-        return "%s(%s:%s)" % (self.__class__.__name__, self.name, self.description)
-AudioOutput._fields_= [
-    ('name', ctypes.c_char_p),
-    ('description', ctypes.c_char_p),
-    ('next', ctypes.POINTER(AudioOutput)),
-    ]
-
-class TrackDescription(ctypes.Structure):
-    def __str__(self):
-        return "%s(%d:%s)" % (self.__class__.__name__, self.id, self.name)
-TrackDescription._fields_= [
-    ('id', ctypes.c_int),
-    ('name', ctypes.c_char_p),
-    ('next', ctypes.POINTER(TrackDescription)),
-    ]
-def track_description_list(head):
-    """Convert a TrackDescription linked list to a python list, and release the linked list.
-    """
-    l = []
-    if head:
-        item = head
-        while item:
-            l.append( (item.contents.id, item.contents.name) )
-            item = item.contents.next
-        libvlc_track_description_release(head)
-    return l
+    def __repr__(self):
+        return '%s.%s' % (self.__class__.__module__, self.__str__())
 
 class MediaEvent(ctypes.Structure):
     _fields_ = [
-        ('media_name', ctypes.c_char_p),
+        ('media_name',    ctypes.c_char_p),
         ('instance_name', ctypes.c_char_p),
-        ]
+    ]
 
-class EventUnion(ctypes.Union):
+class MediaStats(ctypes.Structure):
     _fields_ = [
-        ('meta_type', ctypes.c_uint),
-        ('new_child', ctypes.c_uint),
-        ('new_duration', ctypes.c_longlong),
-        ('new_status', ctypes.c_int),
-        ('media', ctypes.c_void_p),
-        ('new_state', ctypes.c_uint),
-        # Media instance
-        ('new_position', ctypes.c_float),
-        ('new_time', ctypes.c_longlong),
-        ('new_title', ctypes.c_int),
-        ('new_seekable', ctypes.c_longlong),
-        ('new_pausable', ctypes.c_longlong),
-        # FIXME: Skipped MediaList and MediaListView...
-        ('filename', ctypes.c_char_p),
-        ('new_length', ctypes.c_longlong),
-        ('media_event', MediaEvent),
-        ]
+        ('read_bytes',          ctypes.c_int  ),
+        ('input_bitrate',       ctypes.c_float),
+        ('demux_read_bytes',    ctypes.c_int  ),
+        ('demux_bitrate',       ctypes.c_float),
+        ('demux_corrupted',     ctypes.c_int  ),
+        ('demux_discontinuity', ctypes.c_int  ),
+        ('decoded_video',       ctypes.c_int  ),
+        ('decoded_audio',       ctypes.c_int  ),
+        ('displayed_pictures',  ctypes.c_int  ),
+        ('lost_pictures',       ctypes.c_int  ),
+        ('played_abuffers',     ctypes.c_int  ),
+        ('lost_abuffers',       ctypes.c_int  ),
+        ('sent_packets',        ctypes.c_int  ),
+        ('sent_bytes',          ctypes.c_int  ),
+        ('send_bitrate',        ctypes.c_float),
+    ]
 
-class Event(ctypes.Structure):
+    def __str__(self):
+        l = [' %s:\t%s' % (n, getattr(self, n)) for n in self._fields_]
+        return '\n'.join([self.__class__.__name__] + l)
+
+    def __repr__(self):
+        return '%s.%s' % (self.__class__.__module__, self.__str__())
+
+class MediaTrackInfo(ctypes.Structure):
     _fields_ = [
-        ('type', EventType),
-        ('object', ctypes.c_void_p),
-        ('u', EventUnion),
-        ]
+        ('codec',              ctypes.c_uint32),
+        ('id',                 ctypes.c_int   ),
+        ('type',               TrackType      ),
+        ('profile',            ctypes.c_int   ),
+        ('level',              ctypes.c_int   ),
+        ('channels_or_height', ctypes.c_uint  ),
+        ('rate_or_width',      ctypes.c_uint  ),
+    ]
+
+    def __str__(self):
+        l = [" %s:\t%s" % (n, getattr(self, n)) for n in self._fields_]
+        return "\n".join([self.__class__.__name__] + l)
+
+    def __repr__(self):
+        return '%s.%s' % (self.__class__.__module__, self.__str__())
+
+class PlaylistItem(ctypes.Structure):
+    _fields_ = [
+        ('id',   ctypes.c_int   ),
+        ('uri',  ctypes.c_char_p),
+        ('name', ctypes.c_char_p),
+    ]
+
+    def __str__(self):
+        return '%s #%d %s (uri %s)' % (self.__class__.__name__, self.id, self.name, self.uri)
+
+    def __repr__(self):
+        return '%s.%s' % (self.__class__.__module__, self.__str__())
+
+class Position(object):
+    """Enum-like, immutable window position constants.
+
+       See e.g. VideoMarqueeOption.Position.
+    """
+    Center       = 0
+    Left         = 1
+    CenterLeft   = 1
+    Right        = 2
+    CenterRight  = 2
+    Top          = 4
+    TopCenter    = 4
+    TopLeft      = 5
+    TopRight     = 6
+    Bottom       = 8
+    BottomCenter = 8
+    BottomLeft   = 9
+    BottomRight  = 10
+    def __init__(self, *unused):
+        raise TypeError('constants only')
+    def __setattr__(self, *unused):  #PYCHOK expected
+        raise TypeError('immutable constants')
 
 class Rectangle(ctypes.Structure):
     _fields_ = [
@@ -312,26 +304,61 @@ class Rectangle(ctypes.Structure):
         ('left',   ctypes.c_int),
         ('bottom', ctypes.c_int),
         ('right',  ctypes.c_int),
-        ]
+    ]
 
-class Position(object):
-    """Enum-like, position constants for VideoMarqueePosition option.
+class TrackDescription(ctypes.Structure):
+
+    def __str__(self):
+        return '%s(%d:%s)' % (self.__class__.__name__, self.id, self.name)
+
+    def __repr__(self):
+        return '%s.%s' % (self.__class__.__module__, self.__str__())
+
+TrackDescription._fields_ = [  # recursive struct
+        ('id',   ctypes.c_int   ),
+        ('name', ctypes.c_char_p),
+        ('next', ctypes.POINTER(TrackDescription)),
+    ]
+
+def track_description_list(head):
+    """Convert a TrackDescription linked list to a Python list (and release the former).
     """
-    Center=0
-    Left=1
-    CenterLeft=1
-    Right=2
-    CenterRight=2
-    Top=4
-    TopCenter=4
-    TopLeft=5
-    TopRight=6
-    Bottom=8
-    BottomCenter=8
-    BottomLeft=9
-    BottomRight=10
+    r = []
+    if head:
+        item = head
+        while item:
+            item = item.contents
+            r.append((item.id, item.name))
+            item = item.next
+        libvlc_track_description_release(head)
+    return r
 
-    def __init__(self):
-        raise TypeError('Constants only')
+class EventUnion(ctypes.Union):
+    _fields_ = [
+        ('meta_type',    ctypes.c_uint    ),
+        ('new_child',    ctypes.c_uint    ),
+        ('new_duration', ctypes.c_longlong),
+        ('new_status',   ctypes.c_int     ),
+        ('media',        ctypes.c_void_p  ),
+        ('new_state',    ctypes.c_uint    ),
+        # Media instance
+        ('new_position', ctypes.c_float   ),
+        ('new_time',     ctypes.c_longlong),
+        ('new_title',    ctypes.c_int     ),
+        ('new_seekable', ctypes.c_longlong),
+        ('new_pausable', ctypes.c_longlong),
+        # FIXME: Skipped MediaList and MediaListView...
+        ('filename',     ctypes.c_char_p  ),
+        ('new_length',   ctypes.c_longlong),
+        ('media_event',  MediaEvent       ),
+    ]
 
-### End of header.py ###
+class Event(ctypes.Structure):
+    _fields_ = [
+        ('type',   EventType      ),
+        ('object', ctypes.c_void_p),
+        ('u',      EventUnion     ),
+    ]
+
+ # End of header.py #
+
