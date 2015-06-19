1f9fb7d39d01272509eadf9f4b12f19fb4447800
Olivier Aubert <contact@olivieraubert.net>
2015-06-19 13:43:34 +0200

    Fix ctypes.byref reference

--- override.py
+++ override.py
@@ -164,8 +164,8 @@ class Media:
         @version: LibVLC 2.1.0 and later.
         """
         mediaTrack_pp = ctypes.POINTER(MediaTrack)()
-        n = libvlc_media_tracks_get(self, byref(mediaTrack_pp))
-        info = cast(ctypes.mediaTrack_pp, ctypes.POINTER(ctypes.POINTER(MediaTrack) * n))
+        n = libvlc_media_tracks_get(self, ctypes.byref(mediaTrack_pp))
+        info = ctypes.cast(ctypes.mediaTrack_pp, ctypes.POINTER(ctypes.POINTER(MediaTrack) * n))
         return info
 
 class MediaList:
