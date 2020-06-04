93e70b8bd1a95b2ad4ba7305971f69ac342f48ad
albestro <9337627+albestro@users.noreply.github.com>
2020-06-04 15:25:56 +0200

    Fix copyright info

--- examples/play_buffer.py
+++ examples/play_buffer.py
@@ -5,7 +5,7 @@
 
 # MIT License <http://OpenSource.org/licenses/MIT>
 #
-# Copyright (C) 2017-2018 -- mrJean1 at Gmail dot com
+# Copyright (C) 2020 -- A. Invernizzi, @albestro on github
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
