108a8b45bd7928ac151e302ebb40681a9f5fb80d
Yann SALMON <yannsalmon.pro@gmail.com>
2024-01-15 14:47:54 +0100

    utilities for pretty printing a TS node

--- generator/generate.py
+++ generator/generate.py
@@ -196,6 +196,59 @@ def errors(fmt, e=0):
     elif _debug:
         sys.stderr.write(fmt % (_NL_ + 'No') + "\n")
 
+# Adapted from https://gist.github.com/TACIXAT/c5b2db4a80c812c4b4373b65e179a220
+def format_sexp(s_exp, indent_size=4):
+    """Formats a Tree sitter S-expression for better readability.
+    """
+
+    indent_level = 0
+    output = ''
+    i = 0
+    # Initialize to False to avoid newline for the first token
+    need_newline = False
+    cdepth = [] # Track colons
+
+    while i < len(s_exp):
+        if s_exp[i] == '(':
+            if need_newline:
+                  output += '\n'
+            output += ' ' * (indent_level * indent_size) + '('
+            indent_level += 1
+            need_newline = False  # Avoid newline after opening parenthesis
+        elif s_exp[i] == ':':
+            indent_level += 1
+            cdepth.append(indent_level) # Store depth where we saw colon
+            output += ':'
+        elif s_exp[i] == ')':
+            indent_level -= 1
+            if len(cdepth) > 0 and indent_level == cdepth[-1]: 
+                # Unindent when we return to the depth we saw the last colon
+                cdepth.pop()
+                indent_level -= 1
+            output += ')'
+            need_newline = True  # Newline needed after closing parenthesis
+        elif s_exp[i] == ' ':
+            output += ' '
+        else:
+            j = i
+            while j < len(s_exp) and s_exp[j] not in ['(', ')', ' ', ':']:
+                j += 1
+            # Add newline and indentation only when needed
+            if need_newline:
+                output += '\n' + ' ' * (indent_level * indent_size)
+            output += s_exp[i:j]
+            i = j - 1
+            need_newline = True  # Next token should start on a new line
+        i += 1
+
+    return output
+
+def get_tsnode_sexp(tsnode):
+    format_sexp(tsnode.sexp())
+
+def get_tsnode_text(tsnode, encoding ="utf-8"):
+    return tsnode.text.decode(encoding)
+
 class _Source(object):
     """Base class for elements parsed from source.
     """
